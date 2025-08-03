b77cdb247eef09818df3af353b4696ad4ac77f0b
Cavy-cpu <agyemanggodwin77@gmail.com>
2025-08-02 17:47:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".','+",
-    OWNER_NAME: process.env.OWNER_NAME || "Mabhosiedu RAJ👽😎",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
+    OWNER_NAME: process.env.CAVY || "Mabhosiedu RAJ👽😎",
+    NUMERO_OWNER : process.env.233538149721 || " Ibrahim Adams",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'BMW_MD',
